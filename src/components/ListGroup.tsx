40eca760404dcd3fa78b4e5244d2b0a62f2f8645
Sm. Asfakur Rahman Saikat <saikat.hap@gmail.com>
2023-11-23 18:00:03 +0600

    7- Managing State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,26 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function ListGroup() {
   const items = ["Fruits", "Vegetable", "Grocery", "Electronics"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <Fragment>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
