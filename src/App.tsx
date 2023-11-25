fd1f4c797d345e2e9dfe0f32b5c773263479ebab
Asfakur <saikat.hap@gmail.com>
2023-11-25 20:43:11 +0600

    5-11 Sharing State between Components organized

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import NavBarCart from "./components/NavBarCart";
 
 function App() {
-    const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-    return (
-        <div>
-            <NavBar cartItemsCount={cartItems.length} />
-            <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-        </div>
-    );
+    return <NavBarCart />;
 }
 
 export default App;
