6d20091e0d9d2eeb6e390597d6d119296d6bd526
Asfakur <saikat.hap@gmail.com>
2023-11-25 20:52:48 +0600

    5-12 first exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import NavBarCart from "./components/NavBarCart";
+import { useState } from "react";
 
 function App() {
-    return <NavBarCart />;
+    const [game, setGame] = useState({
+        id: 1,
+        player: {
+            name: "John",
+        },
+    });
+    const handleChange = () => {
+        setGame({ ...game, player: { ...game.player, name: "Bob" } });
+    };
+
+    return (
+        <div>
+            <li>Game id: {game.id}</li>
+            <li>Player name: {game.player.name}</li>
+            <button onClick={handleChange}>Change Name</button>
+        </div>
+    );
 }
 
 export default App;
