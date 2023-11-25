2db858444d878eb9fe544969fc34497dbc9b11e0
Asfakur <saikat.hap@gmail.com>
2023-11-25 11:45:34 +0600

    5-6 Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Message from "./Message";
+import { useState } from "react";
 
 function App() {
-   
+    const [drink, setDrink] = useState({
+        title: "Sprite",
+        price: 30,
+    });
+
+    const handleClick = () => {
+        setDrink({...drink, price: ++drink.price})
+    };
+
     return (
         <div>
-            <Message />
+            title: {drink.title}
+            price: {drink.price}
+            <button onClick={handleClick}>Increase Price</button>
         </div>
     );
 }
