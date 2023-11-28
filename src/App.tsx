8fea0976606f0952b6f3ef247b5eea0a11ea8ba9
Asfakur <saikat.hap@gmail.com>
2023-11-28 10:57:05 +0600

    7-2 Understanding the Effect Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import ExpenseTracker from "./expense-tracker/ExpenseTracker";
+import { useEffect, useRef } from "react";
 
 function App() {
+    const ref = useRef<HTMLInputElement>(null);
+
+    // AfterRender
+    useEffect(() => {
+        // side effect
+        if (ref.current) ref.current.focus();
+    })
+
+    useEffect(() => {
+        document.title = 'My App';
+    })
+
     return (
         <div>
-            <ExpenseTracker />
+            <input ref={ref} type="text" className="form-control" />
         </div>
     );
 }
